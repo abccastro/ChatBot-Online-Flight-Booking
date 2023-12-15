8907d7a36b9541c8627bec00bd1d4753763053a5
DESKTOP-RGI068D\acastro <auradeeconcepcion@gmail.com>
2023-12-15 09:10:50 -0500

    cleaned codes for sentiment

--- chatbot codes/functions/flight/flight_sentiment.py
+++ chatbot codes/functions/flight/flight_sentiment.py
@@ -2,22 +2,17 @@ import pandas as pd
 import numpy as np
 import re
 import nltk
-# import matplotlib.pyplot as plt
-# import matplotlib.ticker as mticker
-# import seaborn as sns
-# import warnings
-# flight_sentiment.py
-# from . import text_preprocessing as tp
-# import utils
-# import spacy
-# import seaborn as sb
-# import demoji
-# import contractions
+import text_preprocessing as tp
+import utils
+import spacy
+import seaborn as sb
+import demoji
+import contractions
 
 from nltk.corpus import stopwords
 from nltk.corpus import words
 from nltk.stem import WordNetLemmatizer
-# from flashtext import KeywordProcessor
+from flashtext import KeywordProcessor
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import nltk
 nltk.download('vader_lexicon')
@@ -45,60 +40,45 @@ def generate_vader_labels(text):
     return sentiment
 
 
-# def conduct_text_preprocessing(text):
+def conduct_text_preprocessing(text):
 
-#     try:
-#         # Remove non-grammatical text
-#         text = tp.remove_email_address(input_text)
-#         text = tp.remove_hyperlink(text)
-#         text = tp.replace_html_char(text)
-#         text = tp.remove_attherate(text)
-#         text = tp.remove_hashtag(text)
+    try:
+        # Remove non-grammatical text
+        text = tp.remove_email_address(input_text)
+        text = tp.remove_hyperlink(text)
+        text = tp.replace_html_char(text)
+        text = tp.remove_attherate(text)
+        text = tp.remove_hashtag(text)
 
-#         # Replace emojis in the 'review' column
-#         text = tp.replace_emojis_unicode(text)
-#         text = emoji_dict.replace_keywords(text)
+        # Replace emojis in the 'review' column
+        text = tp.replace_emojis_unicode(text)
+        text = emoji_dict.replace_keywords(text)
 
-#         # Handle contractions
-#         text = contractions.fix(text)
+        # Handle contractions
+        text = contractions.fix(text)
 
-#         # Replace slang words
-#         text = slang_word_dict.replace_keywords(text)
+        # Replace slang words
+        text = slang_word_dict.replace_keywords(text)
 
-#         # Remove leading and trailing whitespaces, and multiple whitespaces
-#         text = text.strip()
-#         text = tp.replace_whitespace(text)
+        # Remove leading and trailing whitespaces, and multiple whitespaces
+        text = text.strip()
+        text = tp.replace_whitespace(text)
 
-#     except Exception as err:
-#         print(f"ERROR: {err}")
-#         print(f"Input Text: {text}")
+    except Exception as err:
+        print(f"ERROR: {err}")
+        print(f"Input Text: {text}")
 
-#     return text
-
-
-# if __name__ == '__main__':
+    return text
 
 
-# try:
 def start_sentiment(review):
-    input_text = review
-# return input_text
-# input_text = input(f"Customer review: ").strip()
+    """
+    Function to clean and generate sentiment for the review
+    """
+    slang_word_dict = tp.get_slang_words(webscraped=True)
+    emoji_dict = tp.get_emojis()
 
-    # slang_word_dict = tp.get_slang_words(webscraped=True)
-    # emoji_dict = tp.get_emojis()
+    preprocessed_text = conduct_text_preprocessing(review)
+    sentiment = generate_vader_labels(review)
 
-    # preprocessed_text = conduct_text_preprocessing(input_text.strip())
-    # sentiment = generate_vader_labels(preprocessed_text)
-    sentiment = generate_vader_labels(input_text)
-
-    # NOTE: The output of this should be put in sentiment column of the review
-    # before saving it in database
-    if sentiment:
-        return sentiment
-    else:
-        return "No Sentinment"
-    # print(f"\nSentiment: {sentiment}")
-
-# except Exception as err:
-#     return err
+    return sentiment
